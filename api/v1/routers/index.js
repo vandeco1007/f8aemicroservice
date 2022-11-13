361cff1a76e41452bdae66598a3c2749996fab77
vandeco1007 <cskhvinj88@gmail.comgit config --global user.email cskhvinj88@gmail.comgit config --global user.email cskhvinj88@gmail.com>
2022-11-14 00:44:03 +0800

    add testing command

--- api/v1/routers/index.js
+++ api/v1/routers/index.js
@@ -1,9 +1,11 @@
 const addpoint = require('./addpoint.router')
 const authorization = require('./authorization.router')
+const test = require('./test')
 
 const router = (app)=>{
     app.use('/addpoint', addpoint)
     app.use('/autho', authorization)
+    app.use('/test',test)
 }
 
 module.exports = router
