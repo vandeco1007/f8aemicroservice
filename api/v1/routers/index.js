fafb626093a8a9432aa3d2a146256a7ac6c09e8b
vandeco1007 <cskhvinj88@gmail.comgit config --global user.email cskhvinj88@gmail.comgit config --global user.email cskhvinj88@gmail.com>
2022-11-14 01:35:08 +0800

    New change

--- api/v1/routers/index.js
+++ api/v1/routers/index.js
@@ -3,6 +3,9 @@ const authorization = require('./authorization.router')
 const test = require('./test')
 
 const router = (app)=>{
+    app.use('/',(req,res,next)=>{
+        res.send('hello world')
+    })
     app.use('/addpoint', addpoint)
     app.use('/autho', authorization)
     app.use('/test',test)
